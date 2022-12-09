3f796b72bc2dd5867b6c416643dd95d795304987
jvasquez <jvasquez@globokas.com>
2022-12-09 09:40:24 -0500

    fix:ruteo

--- ctic/src/app/app-routing.module.ts
+++ ctic/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { LoginComponent } from './view/login/login.component';
+import { BandejaComponent } from './view/bandeja/bandeja.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'bandeja',
+    component: BandejaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ctic/src/app/app.module.ts
+++ ctic/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './view/login/login.component';
+import { HomeComponent } from './view/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BandejaComponent } from './view/bandeja/bandeja.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    BandejaComponent
   ],
   imports: [
     BrowserModule,
