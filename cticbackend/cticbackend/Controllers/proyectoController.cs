dbe35f1f6eca558d6d5587ac8c1a5ed4c78040c7
josechafloque <jose.chafloque.o@uni.pe>
2022-12-12 21:25:33 -0500

    backend

---
+++ cticbackend/cticbackend/Controllers/proyectoController.cs
@@ -0,0 +1,64 @@
+﻿using cticbackend.data.repositorio.data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using cticbackend.model;
+
+namespace cticbackend.Controllers
+{
+    [Route("api/Controller/Proyectos")]
+    [ApiController]
+    public class proyectoController : Controller
+    {
+
+        public readonly interface_data _proyectoRepository;
+
+        public proyectoController(interface_data proyectoRepository)
+        {
+            _proyectoRepository = proyectoRepository;
+        }
+
+        [HttpGet("Lista_Proyectos")]
+        public async Task<IActionResult> ObtenerProyectos()
+        {
+            return Ok(await _proyectoRepository.Obtener_Lista_Proyectos());
+        }
+
+        [HttpPost("Ingresar_Proyecto")]
+        public async Task<IActionResult> IngresarProyecto([FromBody] proyecto proyecto)
+        {
+            if (proyecto == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var created = await _proyectoRepository.Ingresar_Proyecto(proyecto);
+
+            return Created("created", created);
+        }
+
+        [HttpPut("Actualizar_Proyecto")]
+        public async Task<IActionResult> ActualizarProyecto([FromBody] proyecto proyecto)
+        {
+            if (proyecto == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _proyectoRepository.Actualizar_Proyecto(proyecto);
+
+            return NoContent();
+        }
+
+        [HttpDelete("Eliminar_Proyecto{id}")]
+        public async Task<IActionResult> EliminarProyecta(int id)
+        {
+            await _proyectoRepository.Eliminar_Proyecto(new proyecto { id = id });
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ cticbackend/cticbackend/Controllers/usuarioController.cs
@@ -0,0 +1,69 @@
+﻿using cticbackend.data.repositorio.data;
+using cticbackend.model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace cticbackend.Controllers
+{
+    [Route("api/Controller/Usuarios")]
+    [ApiController]
+    public class usuarioController : Controller
+    {
+        public readonly interface_data _usuarioRepository;
+
+        public usuarioController(interface_data usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        [HttpGet("Lista_Usuarios")]
+        public async Task<IActionResult> ObtenerUsuarios()
+        {
+            return Ok(await _usuarioRepository.Obtener_Lista_Usuarios());
+        }
+
+        [HttpGet("Autenticar_Usuario {id},{correo},{contrasena}")]
+        public async Task<IActionResult> Autenticacion_Contrasena(int id, string correo, string contrasena)
+        {
+            return Ok(await _usuarioRepository.Autenticacion_Usuario(id,correo,contrasena));
+        }
+
+        [HttpPost("Ingresar_Usuario")]
+        public async Task<IActionResult> IngresarUsuario([FromBody] usuario usuario)
+        {
+            if (usuario == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var created = await _usuarioRepository.Ingresar_Usuario(usuario);
+
+            return Created("created", created);
+        }
+
+        [HttpPut("Actualizar_Usuario")]
+        public async Task<IActionResult> ActualizarUsuario([FromBody] usuario usuario)
+        {
+            if (usuario == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _usuarioRepository.Actualizar_Usuario(usuario);
+
+            return NoContent();
+        }
+
+        [HttpDelete("Eliminar_Usuario{id}")]
+        public async Task<IActionResult> EliminarUsuario(int id)
+        {
+            await _usuarioRepository.Borrar_Usuario(new usuario {id = id});
+
+            return NoContent();
+        }
+
+    }
+}
