370e025a7b6d71d4610c78878b4d737dd319aa7d
josechafloque <jose.chafloque.o@uni.pe>
2022-12-15 00:52:56 -0500

    usuario

--- ctic/src/app/view/bandeja/bandeja.component.ts
+++ ctic/src/app/view/bandeja/bandeja.component.ts
@@ -4,7 +4,6 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Usuario } from 'src/app/interfaces/usuario';
-import { UsuarioService } from 'src/app/services/usuario.service';
 
 
 @Component({
@@ -13,59 +12,21 @@ import { UsuarioService } from 'src/app/services/usuario.service';
   styleUrls: ['./bandeja.component.css'],
 })
 export class BandejaComponent implements OnInit, AfterViewInit  {
-  displayedColumns: string[] = ['Id', 'nombre', 'apellido', 'dni', 'correo','contrasena','telefono','rol' , 'acciones'];
-  dataSource = new MatTableDataSource<Usuario>();
-  loading: boolean = false;
   
-  @ViewChild(MatPaginator) paginator!: MatPaginator
-  @ViewChild(MatSort) sort!: MatSort;
-  
-  constructor(private _snackBar: MatSnackBar, 
-            private _usuarioService:UsuarioService) { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.obtenerUsuarios();
+    
   }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-    if(this.dataSource.data.length > 0) {
-      this.paginator._intl.itemsPerPageLabel = 'Items por pagina'
-    }
+    
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  applyFilter() {
+    
   }
 
-  obtenerUsuarios() {
-
-    this.loading = true;
-    this._usuarioService.getListUsuarios().subscribe(data => {
-      this.loading = false;
-      this.dataSource.data = data;
-    })
-  }
- 
-
-  eliminarUsuario(id: number) {
-    this.loading = true;
-
-    this._usuarioService.eliminar(id).subscribe(() => {
-     this.mensajeExito();
-     this.loading = false;
-     this.obtenerUsuarios();
-    });    
-  }
-
-  mensajeExito() {
-    this._snackBar.open('El usuario ha sido agregado con exito','', {
-      duration: 4000,
-      horizontalPosition: 'right',
-    });
-  }
 }
 
 
